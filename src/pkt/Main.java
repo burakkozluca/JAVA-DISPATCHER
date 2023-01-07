883968e48a2100724360724190f08d0317fd1259
Burak Kozluca <burakkozluca@Burak-MacBook-Pro.local>
2023-01-08 00:20:20 +0300

    yorum satiri eklendi

--- src/pkt/Main.java
+++ src/pkt/Main.java
@@ -3,11 +3,10 @@ import java.util.*;
 import java.io.*;
 public class Main {
     public static void main(String[] args) {
-
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
-            int timer = 0;
-
+            int timer = 0; //Sistem saniyesi
+            //Kuyruklarimiz olusturuldu.
             LinkedList<Process> input_queue = new LinkedList<Process>();
             LinkedList<Process> current_process = new LinkedList<Process>();
             LinkedList<Process> real_time_queue = new LinkedList<Process>();
@@ -17,31 +16,36 @@ public class Main {
             LinkedList<Process> priority_three_queue = new LinkedList<Process>();
             LinkedList<Process> suspend_queue = new LinkedList<Process>();
 
-            int id = 0;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(", ");
-                int arrival_time = Integer.parseInt(parts[0]);
+            int id = 0; //Proses id degiskeni olusturuldu.
+            while ((line = br.readLine()) != null) //Tüm giris.txt satir satir okundu.
+            {
+                String[] parts = line.split(", "); //Her satirdaki veriler , ile ayrildi.
+                int arrival_time = Integer.parseInt(parts[0]); //İlgili argumanlar ilgili degiskenlere atandi.
                 int priority = Integer.parseInt(parts[1]);
                 int process_time = Integer.parseInt(parts[2]);
 
-                Process p = new Process(arrival_time, priority, process_time, id); //saniye ekle
+                Process p = new Process(arrival_time, priority, process_time, id); //Proses olusturuldu
 
-                input_queue.add(p);
+                input_queue.add(p); //olusturulan proses input_queue'ya eklendi.
                 id++;
             }
+            //tum kuyruklar bos olana kadar sistemin calismasini saglayan while
             while (!input_queue.isEmpty() || !real_time_queue.isEmpty() || !user_job_queue.isEmpty() || !priority_one_queue.isEmpty() || !priority_two_queue.isEmpty() || !priority_three_queue.isEmpty() || !current_process.isEmpty()) {
+                //input_queue bossa ve input_queue'nun ilk elemaninin varis zamani sistem saniyesine esit veya kucukse proses ekleme islemi yapilacak
                 while (!input_queue.isEmpty() && input_queue.peek().arrival_time <= timer) {
-
-                    if (input_queue.peek().priority == 0) {
-                        real_time_queue.add(input_queue.peek()); //inputun ilk prosesini reale at
-                        input_queue.remove(); //inputun ilk prosesini sil
-                    } else {
-                        user_job_queue.add(input_queue.peek());
-                        input_queue.remove();
+                    if (input_queue.peek().priority == 0) //oncelik 0'sa prosesi real_time_queue'ya ekle
+                    {
+                        real_time_queue.add(input_queue.peek()); //input_queue'nun ilk prosesini ekle
+                        input_queue.remove(); //eklenen prosesi input_queue'dan sil
+                    } else // diger oncelikleri user_job_queue'ya ekle
+                    {
+                        user_job_queue.add(input_queue.peek()); //input_queue'nun ilk prosesini ekle
+                        input_queue.remove(); //eklenen prosesi input_queue'dan sil
                     }
                 }
-
+                //user_job_queue bos olana kadar devam eden while
                 while (!user_job_queue.isEmpty()) {
+                    //user_job_queue'daki prosesler onceliklerine gore ilgili kuyruklara eklenir.
                     switch (user_job_queue.peek().priority) {
                         case 1:
                             priority_one_queue.add(user_job_queue.peek());
@@ -57,21 +61,25 @@ public class Main {
                             break;
                     }
                 }
+                //Calisan bir proses varsa
                 if (!current_process.isEmpty()) {
+                    //process_time 1 azaltilir ve 0 olursa proses silinir.
                     if (--current_process.peek().process_time == 0) {
                         current_process.peek().remove(timer);
                         current_process.remove();
                     }
-                    else  //process bitmediyse
+                    else  //proses bitmediyse
                     {
+                        //Kuyruklar bos degilse ve curren_process'in onceligi 0'dan farkliysa
                         if ((!input_queue.isEmpty() || !real_time_queue.isEmpty() || !user_job_queue.isEmpty() || !priority_one_queue.isEmpty() || !priority_two_queue.isEmpty() || !priority_three_queue.isEmpty()) && current_process.peek().priority != 0) {
-                            //prosesi ask�ya alma kodu yaz�lacak
-                            if (++current_process.peek().priority > 3) //�ncelik d���rme
+                            //oncelik arttirma ve onceligi 4 olursa oncelik tekrardan 3 yapilir.
+                            if (++current_process.peek().priority > 3)
                             {
                                 current_process.peek().priority = 3;
                             }
-                            suspend_queue.add(current_process.peek());
+                            suspend_queue.add(current_process.peek()); //Prosesi askiya alma
                             current_process.peek().suspend(timer, current_process.peek().id);
+                            //onceligi degisen prosesi yeni kuyruguna ekleme
                             switch (current_process.peek().priority) {
                                 case 1:
                                     priority_one_queue.add(current_process.peek());
@@ -87,7 +95,9 @@ public class Main {
                         }
                     }
                 }
+                //Tum kuyruklardan (current_process haric) biri bos degilse ve current_process bossa
                 if ((!real_time_queue.isEmpty() || !priority_one_queue.isEmpty() || !priority_two_queue.isEmpty() || !priority_three_queue.isEmpty()) && current_process.isEmpty()) {
+                    //ilgili kuyruk bos degilse current_process'e eklenir.
                     if (!real_time_queue.isEmpty()) {
                         current_process.add(real_time_queue.peek());
                         real_time_queue.remove();
@@ -103,24 +113,27 @@ public class Main {
                     }
                 }
                 if (!current_process.isEmpty())
-                    current_process.peek().run(timer);
+                    current_process.peek().run(timer); //Proses calistirilir.
+                //aski kuyrugu bos degilse ve prosesin askiya alinma zamanindan 20 saniye gecmisse
                 if (!suspend_queue.isEmpty() && (timer == suspend_queue.peek().suspend_time + 20)) {
-                    suspend_queue.peek().zaman_asimi(timer, suspend_queue.peek().id);
+                    suspend_queue.peek().zaman_asimi(timer, suspend_queue.peek().id); //proses zaman asimina ugrar.
+                    //zaman asimina ugrayan proses priority_two_queue'dan silinir.
                     for (Iterator<Process> process = priority_two_queue.iterator(); process.hasNext(); ) {
                         Process process1 = process.next();
                         if (process1.id == suspend_queue.peek().id) {
                             process.remove();
                         }
                     }
+                    //zaman asimina ugrayan proses priority_three_queue'dan silinir.
                     for (Iterator<Process> process = priority_three_queue.iterator(); process.hasNext(); ) {
                         Process process1 = process.next();
                         if (process1.id == suspend_queue.peek().id) {
                             process.remove();
                         }
                     }
-                    suspend_queue.remove();
+                    suspend_queue.remove(); //askidaki proses silinir.
                 }
-                timer++;
+                timer++; //sistem saniyesi arttirilir.
             }
         } catch (IOException e) {
             e.printStackTrace();
--- src/pkt/Process.java
+++ src/pkt/Process.java
@@ -6,6 +6,7 @@ public class Process {
     public int id;
     public int baslangic;
     public int suspend_time;
+    //Process Constructor
     public Process(int arrival_time, int priority, int process_time, int id) {
         this.arrival_time = arrival_time;
         this.priority = priority;
@@ -14,18 +15,13 @@ public class Process {
         this.baslangic = process_time;
         this.suspend_time = process_time;
     }
-    public int getId() {
-        return id;
-    }
-    public int getPriority() {
-        return priority;
-    }
+    //process_time donduren fonksiyon
     public int getTime() {
         return process_time;
     }
-
+    //proses calistirma fonksiyonu
     public void run(int saniye) {
-
+        //proses yeni basliyorsa
         if(getTime() == baslangic) {
             System.out.println("\033[32m" + saniye + "sn proses basladi" + "       (id:" + id + "  oncelik:" + priority + "  kalan sure:" + process_time + "sn)" + "\033[0m");
         }
@@ -35,18 +31,19 @@ public class Process {
         }
            
     }
-    public void remove(int saniye)
+    public void remove(int saniye) //proses bitmisse calisan fonksiyon
     {
         System.out.println("\033[31m"+ saniye + "sn proses sonlandi" + "      (id:" + id + "  oncelik:" + priority + "  kalan sure:" + process_time + "sn)"+ "\033[0m");
     }
 
-    public void suspend(int saniye,int suspend_id)
+    public void suspend(int saniye,int suspend_id) //proses askiya alma fonksiyonu
     {
         id = suspend_id;
         System.out.println("\033[34m" + saniye + "sn proses askida" + "        (id:" + id + " oncelik:" + priority + "  kalan sure:" + process_time + "sn)" + "\033[0m");
         suspend_time = saniye;
     }
-    public void zaman_asimi(int saniye, int suspend_id){
+    public void zaman_asimi(int saniye, int suspend_id) //zaman asimi fonksiyonu
+    {
         System.out.println("\033[36m" + saniye + "sn proses Zaman Asimi" + "    (id:" + id + "  oncelik:" + priority + "  kalan sure:" + process_time + "sn)" + "\033[0m");
     }
 }
